53acd9150c99194db14bc7b23311571c6e6b4ca2
Akanle Jeremy <akanlejeremy@gmail.com>
2024-10-29 12:22:01 +0300

    passing

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,12 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((proj) => {
+          return <ProjectItem name={proj.name} about={proj.about} technologies={proj.technologies} key={proj.id}/>
+        })}
+        </div>
     </div>
   );
 }
